2574cf01e224299bedabe407367a50b6934e2061
shubham07h <shubham1609sharma@gmail.com>
2022-11-24 19:25:26 +0530

    weekday

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,4 +1,27 @@
-if [ $(($RANDOM%2)) -eq 0 ]; then
-  echo "Heads"
+echo "Enter a number between 1-7"
+read num
+
+if [ $num -eq 1 ]
+then
+echo "Sunday"
+elif [ $num -eq 2 ]
+then
+echo "Monday"
+elif [ $num -eq 3 ]
+then
+echo "Tuesday"
+elif [ $num -eq 4 ]
+then
+echo "Wednesday"
+elif [ $num -eq 5 ]
+then
+echo "Thursday"
+elif [ $num -eq 6 ]
+then
+echo "Friday"
+elif [ $num -eq 7 ]
+then
+echo "Saturday"
 else
-  echo "
+echo "Invalid input"
+fi
