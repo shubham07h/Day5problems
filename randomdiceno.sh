c869fb21a1481e6f81e84db4f285d75cc1d22486
shubham07h <shubham1609sharma@gmail.com>
2022-11-24 17:57:57 +0530

    Digit values

--- randomdiceno.sh
+++ randomdiceno.sh
@@ -1,9 +1,16 @@
+for i in {1..5}
+do
+  # Get a random 3 digit value
+  random=$( jot -r 1 100 999 )
 
-#num1=$((RANDOM%6+1))
+  # Print the value
+  echo $random
+done
 
-num2=$((RANDOM%6+1))
+# Get the minimum and maximum value
+min=$( sort -n <<< "$( echo $random )" | head -1 )
+max=$( sort -n <<< "$( echo $random )" | tail -1 )
 
-echo "Addition of two random dice number is : "
-
-echo $num1+$num2 | bc
-=========================
+# Print the minimum and maximum value
+echo "Min: $min"
+echo "Max: $max"
