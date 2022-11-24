c5479a1654bc66af01dfc43c6ead9ff038ba3a9c
shubham07h <shubham1609sharma@gmail.com>
2022-11-24 17:35:27 +0530

    Add Dice Number

--- randomdiceno.sh
+++ randomdiceno.sh
@@ -1,4 +1,3 @@
-Add two random dice number
 
 #num1=$((RANDOM%6+1))
 
@@ -7,3 +6,4 @@ num2=$((RANDOM%6+1))
 echo "Addition of two random dice number is : "
 
 echo $num1+$num2 | bc
+=========================
